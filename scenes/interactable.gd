2282ca45723abdf4883deac61c495021dbe6eeeb
Rigo <217056768+Recsaur@users.noreply.github.com>
2026-08-14 12:40:03 +0400

    Added wall, door, and desk textures. Made door work and added collision and animations.

--- scenes/interactable.gd
+++ scenes/interactable.gd
@@ -1,21 +1,27 @@
 extends Area2D
 @onready var sprite = $"../Sprite2D"
 @onready var act_label = $"../Label"
+@onready var col_hitbox = $"../CollisionShape2D"
 var player_near = false
 
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("interact") and player_near:
 		sprite.play("swing_open")
+		col_hitbox.disabled = true
+		sprite.modulate = Color(1,1,1)
+		act_label.hide()
 
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and not col_hitbox.disabled:
 		act_label.show()
+		sprite.modulate = Color(1.572, 1.572, 1.572)
 		player_near = true
 
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		act_label.hide()
+		sprite.modulate = Color(1,1,1)
 		player_near = false
--- scenes/player.gd
+++ scenes/player.gd
@@ -9,24 +9,34 @@ var SPEED = 1000.0
 const JUMP_VELOCITY = -400.0
 const DASH_SPEED = 2250
 var kick = true
+@onready var sprite = $Sprite2D
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("left", "right", "up", "down")
 	#velocity = direction * SPEED
-	if get_global_mouse_position().x > position.x:
-		$Sprite2D.flip_h = false
-	else:
-		$Sprite2D.flip_h = true
+	#if get_global_mouse_position().x > position.x:
+		#sprite.flip_h = true
+	#else:
+		#sprite.flip_h = false
 	#print(direction)
-	match direction:
-		Vector2.LEFT:
-			InteractPivot.rotation_degrees = 90
-		Vector2.RIGHT:
-			InteractPivot.rotation_degrees = 270
-		Vector2.UP:
-			InteractPivot.rotation_degrees = 180
-		Vector2.DOWN:
-			InteractPivot.rotation_degrees = 0
+	if direction != Vector2.ZERO:
+		match direction:
+			Vector2.LEFT:
+				InteractPivot.rotation_degrees = 90
+				sprite.flip_h = false
+				sprite.play("WalkSide")
+			Vector2.RIGHT:
+				InteractPivot.rotation_degrees = 270
+				sprite.flip_h = true
+				sprite.play("WalkSide")
+			Vector2.UP:
+				InteractPivot.rotation_degrees = 180
+				sprite.play("WalkBack")
+			Vector2.DOWN:
+				InteractPivot.rotation_degrees = 0
+				sprite.play("WalkFront")
+	else:
+		sprite.stop()
 			
 	if Input.is_action_just_pressed("interact"):
 		InteractCollision.disabled = false
