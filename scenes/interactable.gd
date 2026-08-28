db6694b441cfe3367a18aaba50852b984494e0d9
Rigo <217056768+Recsaur@users.noreply.github.com>
2026-08-28 17:38:04 +0400

    Added the backrooms and fixed some code

--- scenes/interactable.gd
+++ scenes/interactable.gd
@@ -3,10 +3,11 @@ extends Area2D
 @onready var act_label = $"../Label"
 @onready var col_hitbox = $"../CollisionShape2D"
 var player_near = false
-
+var alrdy_use = false
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("interact") and player_near:
+	if Input.is_action_just_pressed("interact") and player_near and not alrdy_use:
+		alrdy_use = true
 		sprite.play("swing_open")
 		await get_tree().create_timer(0.125).timeout
 		col_hitbox.disabled = true
@@ -34,8 +35,9 @@ func shake_random():
 	
 
 func _on_area_entered(area: Area2D) -> void:
-	if area.is_in_group("kick") and not col_hitbox.disabled:
+	if area.is_in_group("kick") and not col_hitbox.disabled and not alrdy_use:
 		GameController.emit_signal("Action",15)
+		alrdy_use = true
 		sprite.speed_scale = 2
 		sprite.play("swing_open")
 		await get_tree().create_timer(0.05).timeout
--- scenes/interactablesidedoor.gd
+++ scenes/interactablesidedoor.gd
@@ -3,17 +3,19 @@ extends Area2D
 @onready var act_label = $"../Label"
 @onready var col_hitbox = $"../CollisionShape2D"
 var player_near = false
-
+var alrdy_use = false
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("interact") and player_near:
+	if Input.is_action_just_pressed("interact") and player_near and not alrdy_use:
+		alrdy_use = true
 		sprite.play("swing_open")
 		await get_tree().create_timer(0.125).timeout
 		col_hitbox.disabled = true
 		$"../CollisionShape2D2".disabled = false
 		sprite.modulate = Color(1,1,1)
 		act_label.hide()
-
+	#if Input.is_action_just_pressed("paw1bounce")
+	#I had an idea to make like keybind/s for moving the paws up and down so like u can bounce items and play n stuff, but it would be a cool game mechanic for sumn else rightt??? 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player") and not col_hitbox.disabled:
@@ -35,7 +37,8 @@ func shake_random():
 	
 
 func _on_area_entered(area: Area2D) -> void:
-	if area.is_in_group("kick") and not col_hitbox.disabled:
+	if area.is_in_group("kick") and not col_hitbox.disabled and not alrdy_use:
+		alrdy_use = true
 		GameController.emit_signal("Action",15)
 		sprite.speed_scale = 2
 		sprite.play("swing_open")
